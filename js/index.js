ed8b828d6328fb90aa5671d45e9895ee16d202ea
ikhzz <fikriztm@gmail.com>
2020-10-21 19:19:37 +0800

    upload pic and start add desc

--- js/index.js
+++ js/index.js
@@ -1,61 +1,28 @@
 import {IndexedDB, Pages} from './indexedDb.js';
-new IndexedDB();
+const indexedDB = new IndexedDB();
+const pages = new Pages();
 
-document.querySelector('.prev').addEventListener('click', ()=> {
-    document.querySelector('.upload').classList.toggle('uploads')
+document.querySelectorAll('.cancel').forEach(e => {
+  e.addEventListener('click', pages.upToggle)
 })
 
-// const write = () => {
-//     console.log('hi')
-//   const open = indexedDB.open(db_name, db_version)
-//     open.onsuccess = () =>{
-//       const db = open.result,
-//       tx = db.transaction(db_table, 'readwrite'),
-//       request = tx.objectStore(db_table),
-//       image = prev(),
-//       data = {id : 1, profile: image}
-//     request.autoIncrement
-//     request.put(data)
-//     }       
-// }
+document.querySelector('.fp').addEventListener('click', ()=> {
+    document.querySelector('.upload').classList.toggle('uploads') 
+    
+    document.querySelector('#style').addEventListener('change', (e)=> {
+      document.querySelector('.prevProfile').style.borderRadius = e.target.value
+    })
+})
 
-const write = () => {
-  const image = prev()
-  if(image === null) {
-    console.log('null')
-  } else {
-    console.log('not null')
-  }
-  //console.log(image)
-  //console.log(image.type)
-  document.querySelector('.input').value = ''
-  document.querySelector('.upload').classList.toggle('uploads')
+document.querySelector('.confirmProfile').addEventListener('click', indexedDB.addProfilepic)
 
-}
- const input = document.querySelector('.input'),
-       btnwrite = document.querySelector('.write')
+document.querySelector('.addProfile').addEventListener('click', ()=> {
+  document.querySelector('.addDesc').classList.toggle('uploads')
+})
 
-btnwrite.addEventListener('click', write)
-// console.log(input)
+document.querySelector('.inputPic').addEventListener('change', pages.prev)
 
-// input.addEventListener('change', (e)=> {
-//   const file = document.querySelector('.input').files[0];
-  
-//   document.querySelector('.input').value = ''
-// })
-function prev() {
-  const file = document.querySelector('.input').files[0];
-  const filetype = ['image/jpeg', 'image/png', 'image/jpg', 'image/bmp', 'image/gif'];
 
-  if(file !== undefined)
-    if(filetype.includes(file.type)) {
-      return file
-    } else {
-      console.log('false')
-      document.querySelector('.input').value = ''
-      return null
-  } else {
-      return null
-  }
-}
-window.prev = prev
+
+
+
--- js/indexedDb.js
+++ js/indexedDb.js
@@ -1,17 +1,19 @@
 class IndexedDB {
+  _db_name = 'iNotes';
+  _db_version = 1;
+  _db_tOne = 'profile';
+  _reader = new FileReader();
+
   constructor() {
-    this.db_name = 'iNotes';
-    this.db_version = 1;
-    this.db_tOne = 'profile'
     this.startDB()
-    this.loadProfile(document.querySelector('.prev'))
+    this.loadProfile()
   }
 
   startDB = () => {
     if(window.indexedDB) {
-      const open = indexedDB.open(this.db_name, this.db_version);
+      const open = indexedDB.open(this._db_name, this._db_version)
       open.onupgradeneeded = e => {
-        e.target.result.createObjectStore(this.db_table, {keyPath : 'id'});
+        e.target.result.createObjectStore(this._db_tOne, {keyPath : 'id', unique : true});
       }
       open.onsuccess = e => {
         console.log('INDEXEDDB IS INVOKED!!!!!!');
@@ -24,21 +26,46 @@ class IndexedDB {
     }
   }
 
-  loadProfile = (imgTarget) => {
-    const open = indexedDB.open(this.db_name, this.db_version)
+  loadProfile = () => {
+    const open = indexedDB.open(this._db_name, this._db_version)
       open.onsuccess = () =>{
         const db = open.result,
-              tx = db.transaction(this.db_tOne, 'readonly'),
-              store = tx.objectStore(this.db_tOne),
-              request = store.get(1)
+              tx = db.transaction(this._db_tOne, 'readonly'),
+              store = tx.objectStore(this._db_tOne),
+              request = store.get('profPic')
         request.onsuccess = e => {
-          const reader = new FileReader()
-          reader.readAsDataURL(e.target.result.profile)
-          reader.onload = e => {
-            imgTarget.setAttribute('src', e.target.result)
+          this._reader.readAsDataURL(e.target.result.profile)
+          this._reader.onload = e => {
+            document.querySelector('.fp').setAttribute('src', e.target.result)
+            
           }
+          document.querySelector('.fp').style.borderRadius = e.target.result.style
         }
       }       
+  }
+
+  addProfilepic = () => {
+    const file = document.querySelector('.inputPic').files[0],
+          filetype = ['image/jpeg', 'image/png', 'image/jpg', 'image/bmp', 'image/gif'],
+          pages = new Pages
+
+    if(file !== undefined)
+      if(filetype.includes(file.type)) {
+        const open = indexedDB.open(this._db_name, this._db_version)
+        open.onsuccess = () =>{
+          const db = open.result,
+                tx = db.transaction(this._db_tOne, 'readwrite'),
+                request = tx.objectStore(this._db_tOne),
+                style = document.querySelector('#style').value,
+                data = {id : 'profPic', profile: file, style : style}
+        request.put(data)
+        }
+      } else {
+        console.log('false')
+        pages.upToggle()
+      }
+    this.loadProfile()
+    pages.upToggle('upload')
   }
 }
 
@@ -50,5 +77,35 @@ class Pages extends IndexedDB {
           document.querySelector('.content').innerHTML = res
     })
   }
+
+  upToggle = () => {
+    document.querySelectorAll('.option>div').forEach(e => {
+      if(e.classList.contains('uploads'))
+        e.classList.toggle('uploads')
+    })
+    if(document.querySelector('.prevProfile')) {
+      document.querySelector('.prevProfile').setAttribute('src','')
+      document.querySelector('.inputPic').value = '';
+    }
+  }
+
+  prev = () => {
+    const file = document.querySelector('.inputPic').files[0],
+          filetype = ['image/jpeg', 'image/png', 'image/jpg', 'image/bmp', 'image/gif'];
+
+    if(file !== undefined) 
+      if(filetype.includes(file.type)) {
+        const reader = new FileReader()
+              reader.readAsDataURL(file)
+              reader.onload = e => {
+                document.querySelector('.prevProfile').setAttribute('src', e.target.result)
+              }
+      } else {
+        console.log('false')
+        document.querySelector('.inputPic').value = ''
+        document.querySelector('.prevProfile').setAttribute('src','')
+      }
+  }
 }
+
 export {IndexedDB, Pages}
